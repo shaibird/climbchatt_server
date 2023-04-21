2218455b2ef3162687074132d36265137e1eea6b
Shaina Couch <shainacouch21@gmail.com>
2023-04-21 17:47:42 -0500

    adding logic to authenticate login

--- chattclimb/urls.py
+++ chattclimb/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from chattclimbapi.views import login_user, register_user
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('login', login_user),
+    path('register', register_user),
+    path('', include(router.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
