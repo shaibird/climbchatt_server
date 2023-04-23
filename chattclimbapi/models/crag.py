23bdfb3ef15cbe538bcc6becdf5011883b167663
Shaina Couch <shainacouch21@gmail.com>
2023-04-23 09:03:50 -0500

    updating crag model and adding fixture

--- chattclimbapi/models/crag.py
+++ chattclimbapi/models/crag.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Crag(models.Model):
     name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    state = models.ForeignKey("State", on_delete=models.CASCADE, related_name="crag_state")
     description = models.CharField(max_length=400)
     type = models.ForeignKey("Crag", on_delete=models.CASCADE, related_name="crag_type")
+    access = models.CharField(max_length=500)ß
